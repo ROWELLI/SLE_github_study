18a475a3ec4d0cca49e480d98236b8c66e61919a
LeeOnYu <loy7153@gmail.com>
2024-11-05 15:14:41 +0900

    create 2

--- hello.c
+++ hello.c
@@ -2,10 +2,6 @@
 
 int main(){
     printf("Hello World!");
-<<<<<<< HEAD
-    printf("github");
-=======
-    printf("github branch 1 testing");
->>>>>>> eec5d5a548f18971e1022cf8b203c01cc983675b
+    printf("github branch 2 testing");
     printf("hi");
 }
